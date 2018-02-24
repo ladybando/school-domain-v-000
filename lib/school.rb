a43958c9ec8eb94771dc74cb3573da0247abe987
Ayana Bando <ayanaflax@yahoo.com>
2018-02-24 15:00:13 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,10 @@
 # code here!
 class School
   attr_accessor :name, :roster
-  
+
   def initialize(name)
     @name = name
+    @roster={}
   end
   def add_student(add_student, grade)
         Hash.new { |add_student, grade| add_student[grade] = [] }
